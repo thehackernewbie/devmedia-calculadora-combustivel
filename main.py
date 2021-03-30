cc9b7daa2c1f989ad1710ed6875755b4c6fa38d6
Hacker Newbie <63235365+thehackernewbie@users.noreply.github.com>
2021-03-30 05:52:23 -0300

    Cria funcão main
    Declara o contéudo do módulo principal dentro de uma funcão

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+
+    print(
+        """
+        Essa aplicao tem como finalidade demonstrar os valores que serao gastos
+        com combustivel durante uma viagem, com base no contador so seu veiculo, e
+        com a distancia determinada por voce
+        """
+    )
+
+    print("Os combustiveis disponiveis para esse calulo sao:")
+    print("      Alcool")
+    print("      Diesel")
+    print("      Gasolina")
+
+    print("    ")
